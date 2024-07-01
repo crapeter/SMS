456baa66abee8e7f1e5a5c92abfe0f9923f5a33c
crapeter <craig.peterson0701@gmail.com>
2024-07-01 13:46:21 -0500

    JSON properties to DTO

--- PostgreSQL_SMS/src/main/java/crapeter/proj/PostgreSQL_SMS/Model/StudentInfoDTO.java
+++ PostgreSQL_SMS/src/main/java/crapeter/proj/PostgreSQL_SMS/Model/StudentInfoDTO.java
@@ -1,15 +1,31 @@
 package crapeter.proj.PostgreSQL_SMS.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class StudentInfoDTO {
+  @JsonProperty("username")
   private String username;
+
+  @JsonProperty("firstName")
   private String firstName;
+
+  @JsonProperty("lastName")
   private String lastName;
+
+  @JsonProperty("mathGrade")
   private Integer mathGrade;
+
+  @JsonProperty("elaGrade")
   private Integer elaGrade;
+
+  @JsonProperty("scienceGrade")
   private Integer scienceGrade;
+
+  @JsonProperty("historyGrade")
   private Integer historyGrade;
+
+  @JsonProperty("readingGrade")
   private Integer readingGrade;
 }
